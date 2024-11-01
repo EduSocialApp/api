a5473f78b5b992f320138c5f5fd93e674c3b95bb
Felipe V. Sobral <xfelipesobral@gmail.com>
2024-11-01 17:08:55 -0300

    eventos

--- src/modules/organization/http/findOrganizationById.ts
+++ src/modules/organization/http/findOrganizationById.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 
 import organization from '../organization.service'
+import organizationMember from '../member/organizationmember.service'
 
 /**
  * Retorna informações básicas de uma organização
@@ -9,13 +10,25 @@ export default async function findOrganizationById(request: Request, response: R
     try {
         const organizationData = await organization.findByIdWithAddresses(request.params.id)
 
+        const totalMembers = await organization.getTotalMembers(request.params.id)
+        const totalLikes = await organization.getTotalLikes(request.params.id)
+
+        const organizationDataWithStats = {
+            ...organizationData,
+            stats: {
+                members: totalMembers,
+                likes: totalLikes,
+                medals: 0,
+            },
+        }
+
         if (request.user.role === 'ADMIN' || request.user.role === 'MODERATOR') {
             const owners = await organization.getOwners(request.params.id)
 
-            return response.status(200).json({ ...organizationData, owners: owners?.members || [] })
+            return response.status(200).json({ ...organizationDataWithStats, owners: owners?.members || [] })
         }
 
-        response.status(200).json(organizationData)
+        response.status(200).json(organizationDataWithStats)
     } catch (e) {
         next(e)
     }
--- src/modules/organization/organization.service.ts
+++ src/modules/organization/organization.service.ts
@@ -2,12 +2,21 @@ import OrganizationController from './organization.controller'
 
 import organizationAdress from '../address/organization/organizationaddress.service'
 import organizationMember from './member/organizationmember.service'
+import postService from '../post/post.service'
 
 export class Organization extends OrganizationController {
     async fullDelete(id: string) {
         await organizationMember.deleteAllLinksByOrganizationId(id) // deleta todos os membros da organização
         await organizationAdress.deleteAdressesOrganization(id) // deleta todos os endereços da organização
         await this.delete(id) // deleta organização
+    }
+
+    getTotalLikes(id: string) {
+        return postService.countLikesByOrganizationId(id)
+    }
+
+    getTotalMembers(id: string) {
+        return organizationMember.countByOrganizationId(id)
     }
 }
 
