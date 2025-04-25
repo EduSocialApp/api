749c7425786f32480c398a302e67113a4a7c132b
Felipe V. Sobral <xfelipesobral@gmail.com>
2025-04-25 17:56:40 -0300

    mudando onde chama o server

--- index.ts
+++
@@ -1,8 +0,0 @@
-import { config as configDotEnv } from 'dotenv'
-import { startServer } from './src/server'
-
-configDotEnv()
-
-startServer({
-    serverPort: Number(process.env.EDUSOCIAL_PORT_BACKEND) || 3000
-})
